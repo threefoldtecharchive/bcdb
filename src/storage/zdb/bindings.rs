a37d1f3f4959cf7525ebf4c2cdd9e0921316137f
LeeSmet <lee.smet@hotmail.com>
2020-04-02 22:53:17 +0200

    Ignore more warnings in generated bindings

--- src/storage/zdb/bindings.rs
+++ src/storage/zdb/bindings.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)] // TODO: see if we can reduce generated binding size
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/libzdb_bindings.rs"));
