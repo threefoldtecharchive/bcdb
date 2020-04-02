b06acd34b6d420d67de95d965e3c5ac0c4e99425
Muhamad Azamy <muhamad@incubaid.com>
2020-04-02 13:49:40 +0200

    Start bcdb server, and test a go client

--- src/bcdb/mod.rs
+++ src/bcdb/mod.rs
@@ -1,30 +1,32 @@
-use generated::bcdb_server::{Bcdb, BcdbServer};
+use generated::bcdb_server::Bcdb;
 use generated::*;
 use tokio::sync::mpsc;
 use tonic::{transport::Server, Request, Response, Status};
+
+pub use generated::bcdb_server::BcdbServer;
 
 pub mod generated {
     tonic::include_proto!("bcdb"); // The string specified here must match the proto package name
 }
 
 #[derive(Default)]
-struct BcdbService;
+pub struct BcdbService;
 
 #[tonic::async_trait]
 impl Bcdb for BcdbService {
     async fn set(&self, request: Request<SetRequest>) -> Result<Response<SetResponse>, Status> {
-        unimplemented!()
+        Err(Status::unimplemented("not implemented yet!"))
     }
 
     async fn get(&self, request: Request<GetRequest>) -> Result<Response<GetResponse>, Status> {
-        unimplemented!()
+        Err(Status::unimplemented("not implemented yet!"))
     }
 
     async fn update(
         &self,
         request: Request<UpdateRequest>,
     ) -> Result<Response<UpdateResponse>, Status> {
-        unimplemented!()
+        Err(Status::unimplemented("not implemented yet!"))
     }
 
     type ListStream = mpsc::Receiver<Result<ListResponse, Status>>;
@@ -33,7 +35,7 @@ impl Bcdb for BcdbService {
         &self,
         request: Request<QueryRequest>,
     ) -> Result<Response<Self::ListStream>, Status> {
-        unimplemented!()
+        Err(Status::unimplemented("not implemented yet!"))
     }
 
     type FindStream = mpsc::Receiver<Result<FindResponse, Status>>;
@@ -42,6 +44,6 @@ impl Bcdb for BcdbService {
         &self,
         request: Request<QueryRequest>,
     ) -> Result<Response<Self::FindStream>, Status> {
-        unimplemented!()
+        Err(Status::unimplemented("not implemented yet!"))
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,16 @@
+use tonic::transport::Server;
+
 mod bcdb;
 
-fn main() {
-    println!("hello");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let service = bcdb::BcdbService::default();
+
+    Server::builder()
+        .add_service(bcdb::BcdbServer::new(service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
