0a2793a0f10e56d84e01a7ad1892d5d71fb5070a
LeeSmet <lee.smet@hotmail.com>
2020-04-09 13:02:13 +0200

    Hook up set and get methods to actual backend

--- src/bcdb/mod.rs
+++ src/bcdb/mod.rs
@@ -1,7 +1,10 @@
 use generated::bcdb_server::Bcdb;
 use generated::*;
 use tokio::sync::mpsc;
+use tokio::task::spawn_blocking;
 use tonic::{Request, Response, Status};
+
+use crate::storage::{zdb::Zdb, Storage};
 
 pub use generated::bcdb_server::BcdbServer;
 
@@ -10,37 +13,57 @@ pub mod generated {
 }
 
 #[derive(Default)]
-pub struct BcdbService;
+pub struct BcdbService {
+    db: Zdb,
+}
 
 #[tonic::async_trait]
 impl Bcdb for BcdbService {
     async fn set(&self, request: Request<SetRequest>) -> Result<Response<SetResponse>, Status> {
         let request = request.into_inner();
-        let tags = match request.metadata {
-            Some(metadata) => metadata.tags,
-            None => vec![],
-        };
 
-        for t in tags.iter() {
-            println!("Tag: {}", t.key);
-            if let Some(ref value) = t.value {
-                match value {
-                    tag::Value::String(v) => println!("\tstring: {}", v),
-                    tag::Value::Double(v) => println!("\tdouble: {}", v),
-                    tag::Value::Unsigned(v) => println!("\tunsigned: {}", v),
-                    tag::Value::Number(v) => println!("\tnumber: {}", v),
+        if let Some(ref metadata) = request.metadata {
+            for t in metadata.tags.iter() {
+                println!("Tag: {}", t.key);
+                if let Some(ref value) = t.value {
+                    match value {
+                        tag::Value::String(v) => println!("\tstring: {}", v),
+                        tag::Value::Double(v) => println!("\tdouble: {}", v),
+                        tag::Value::Unsigned(v) => println!("\tunsigned: {}", v),
+                        tag::Value::Number(v) => println!("\tnumber: {}", v),
+                    }
                 }
             }
         }
 
-        println!("data {:?}", std::str::from_utf8(&request.data));
+        // TODO: create from impl for Tonic status for StorageError
+        // the problem with this clone is that it moves a ref to self in the closure,
+        // which forces us to mess with the lifetimes. Since the clone actually opens a new
+        // connection, it is not cheap and blocks the reactor. Need to find a way to solve this.
+        let mut db = self.db.clone();
+        let handle =
+            spawn_blocking(move || db.set(None, &request.data).expect("failed to set data"));
+        // TODO: proper error
+        let id = handle.await.expect("failed to run blocking task");
 
-        Ok(Response::new(SetResponse { id: "test".into() }))
-        //Err(Status::unimplemented("not implemented yet!"))
+        Ok(Response::new(SetResponse { id }))
     }
 
     async fn get(&self, request: Request<GetRequest>) -> Result<Response<GetResponse>, Status> {
-        Err(Status::unimplemented("not implemented yet!"))
+        let id = request.into_inner().id;
+
+        // TODO: from impl for error
+        let mut db = self.db.clone();
+        let handle = spawn_blocking(move || db.get(id).expect("failed to load data"));
+        // TODO: proper error handling
+        let data = handle.await.expect("failed threadpool task");
+        if data.is_none() {
+            return Err(Status::not_found(format!("Key {} doesn't exist", id)));
+        }
+        Ok(Response::new(GetResponse {
+            data: data.unwrap(), // This unwrap is safe as we checked the none case above
+            metadata: None,
+        }))
     }
 
     async fn update(
@@ -60,11 +83,7 @@ impl Bcdb for BcdbService {
 
         tokio::spawn(async move {
             for i in 0..3 {
-                tx.send(Ok(ListResponse {
-                    id: format!("{}", i),
-                }))
-                .await
-                .unwrap();
+                tx.send(Ok(ListResponse { id: i })).await.unwrap();
             }
         });
 
