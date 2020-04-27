e9ab08e430235dcd2759a395009f60b2e9079f40
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-04-27 16:17:52 +0200

    move get_auth_header to AuthGateway...
    also remove extra spaces from header value

--- clients/python/bcdb/auth.py
+++ clients/python/bcdb/auth.py
@@ -18,26 +18,25 @@ class AuthOptions(NamedTuple):
 EnMnemonic = Mnemonic("english")
 
 
-def get_auth_header(options):
-    created = int(time.time())
-    expires = created + options.expires
-
-    headers = f"(created): {created}\n"
-    headers += f"(expires): {expires}\n"
-    headers += f"(key-id): {options.id}"
-
-    signed_headers = options.key.sign(headers.encode())
-    signature = base64.standard_b64encode(signed_headers.signature).decode()
-
-    auth_header = f'Signature keyId="{options.id}",algorithm="hs2019",created="{created}",expires="{expires}",headers="(created) (expires) (key-id) ", signature="{signature}"'
-    return "authorization", auth_header
-
-
 # from https://github.com/grpc/grpc/tree/master/examples/python/auth
 class AuthGateway(grpc.AuthMetadataPlugin):
     def __init__(self, auth_options, *args, **kwargs):
         self.auth_options = auth_options
         super().__init__(*args, **kwargs)
+
+    def get_auth_header(self):
+        created = int(time.time())
+        expires = created + self.auth_options.expires
+
+        headers = f"(created): {created}\n"
+        headers += f"(expires): {expires}\n"
+        headers += f"(key-id): {self.auth_options.id}"
+
+        signed_headers = self.auth_options.key.sign(headers.encode())
+        signature = base64.standard_b64encode(signed_headers.signature).decode()
+
+        auth_header = f'Signature keyId="{self.auth_options.id}",algorithm="hs2019",created="{created}",expires="{expires}",headers="(created) (expires) (key-id)",signature="{signature}"'
+        return "authorization", auth_header
 
     def __call__(self, context, callback):
         """Implements authentication by passing metadata to a callback.
@@ -48,11 +47,10 @@ class AuthGateway(grpc.AuthMetadataPlugin):
         callback: An AuthMetadataPluginCallback to be invoked either
             synchronously or asynchronously.
         """
-        header = get_auth_header(self.auth_options)
-        callback((header,), None)
+        callback((self.get_auth_header(),), None)
 
 
-def auth_plugin_from_words(user_id, words, expires=3600):
+def auth_plugin_from_words(user_id, words, expires=3):
     entropy = EnMnemonic.to_entropy(words)
     signing_key = SigningKey(bytes(entropy))
     options = AuthOptions(user_id, signing_key, expires)
