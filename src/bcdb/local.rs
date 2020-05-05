a227ffaa08fe3116499c3d6aaef8697eeed00a8d
Muhamad Azamy <muhamad@incubaid.com>
2020-05-05 11:51:43 +0200

    Before breaking everything

--- src/bcdb/local.rs
+++ src/bcdb/local.rs
@@ -1,43 +1,21 @@
+use super::generated::bcdb_server::Bcdb as BcdbServiceTrait;
+use super::generated::*;
+use super::FailureExt;
 use crate::acl::*;
 use failure::Error;
-use generated::acl_server::Acl as AclServiceTrait;
-use generated::bcdb_server::Bcdb as BcdbServiceTrait;
-use generated::*;
 use std::collections::HashMap;
-use std::collections::HashSet;
-use std::iter::FromIterator;
 use std::sync::Arc;
 use tokio::sync::mpsc;
 use tokio::sync::Mutex;
 use tokio::task::spawn_blocking;
-use tonic::{Code, Request, Response, Status};
+use tonic::{Request, Response, Status};
 
 use crate::auth::MetadataMapAuth;
 use crate::meta::{self, Storage as MetaStorage, StorageFactory as MetaStorageFactory};
-use crate::storage::{zdb::Collection, zdb::Zdb, Storage as ObjectStorage};
-
-pub use generated::acl_server::AclServer;
-pub use generated::bcdb_server::BcdbServer;
-
-pub mod generated {
-    tonic::include_proto!("bcdb"); // The string specified here must match the proto package name
-}
-
-mod local;
-pub use local::LocalBcdb;
-
-trait FailureExt {
-    fn status(&self) -> Status;
-}
-
-impl FailureExt for Error {
-    fn status(&self) -> Status {
-        Status::new(Code::Internal, format!("{}", self))
-    }
-}
+use crate::storage::Storage as ObjectStorage;
 
 //TODO: use generics for both object store type and meta factory type.
-pub struct BcdbService<S, M>
+pub struct LocalBcdb<S, M>
 where
     S: ObjectStorage,
     M: MetaStorageFactory,
@@ -49,13 +27,13 @@ where
     acl: ACLStorage<S>,
 }
 
-impl<S, M> BcdbService<S, M>
+impl<S, M> LocalBcdb<S, M>
 where
     S: ObjectStorage,
     M: MetaStorageFactory,
 {
-    pub fn new(db: S, factory: M, acl: ACLStorage<S>) -> BcdbService<S, M> {
-        BcdbService {
+    pub fn new(db: S, factory: M, acl: ACLStorage<S>) -> LocalBcdb<S, M> {
+        LocalBcdb {
             db,
             factory,
             collections: Arc::new(Mutex::new(HashMap::new())),
@@ -168,7 +146,7 @@ where
 }
 
 #[tonic::async_trait]
-impl<S, M> BcdbServiceTrait for BcdbService<S, M>
+impl<S, M> BcdbServiceTrait for LocalBcdb<S, M>
 where
     S: ObjectStorage + Send + Sync + 'static,
     M: MetaStorageFactory,
@@ -494,267 +472,5 @@ where
         });
 
         Ok(Response::new(rx))
-    }
-}
-
-pub struct AclService<S>
-where
-    S: ObjectStorage,
-{
-    store: ACLStorage<S>,
-}
-
-impl Default for AclService<Collection> {
-    fn default() -> AclService<Collection> {
-        AclService {
-            store: ACLStorage::new(Zdb::default().collection("acl")),
-        }
-    }
-}
-
-impl<S> AclService<S>
-where
-    S: ObjectStorage,
-{
-    pub fn new(store: ACLStorage<S>) -> AclService<S> {
-        AclService { store: store }
-    }
-}
-
-#[tonic::async_trait]
-impl<S> AclServiceTrait for AclService<S>
-where
-    S: ObjectStorage + Send + Sync + 'static,
-{
-    async fn get(
-        &self,
-        request: Request<AclGetRequest>,
-    ) -> Result<Response<AclGetResponse>, Status> {
-        let meta = request.metadata();
-
-        if !meta.is_owner() {
-            return Err(Status::unauthenticated("not authorized"));
-        }
-
-        let request = request.into_inner();
-        let mut store = self.store.clone();
-
-        let acl = match store.get(request.key) {
-            Ok(acl) => match acl {
-                Some(acl) => acl,
-                None => return Err(Status::not_found("acl not found")),
-            },
-            Err(err) => {
-                return Err(err.status());
-            }
-        };
-
-        Ok(Response::new(AclGetResponse {
-            acl: Some(Acl {
-                perm: acl.perm.to_string(),
-                users: acl.users,
-            }),
-        }))
-    }
-
-    async fn create(
-        &self,
-        request: Request<AclCreateRequest>,
-    ) -> Result<Response<AclCreateResponse>, Status> {
-        let meta = request.metadata();
-
-        if !meta.is_owner() {
-            return Err(Status::unauthenticated("not authorized"));
-        }
-
-        let request = match request.into_inner().acl {
-            Some(request) => request,
-            None => return Err(Status::invalid_argument("missing acl")),
-        };
-
-        let set: HashSet<u64> = HashSet::from_iter(request.users);
-
-        let acl = ACL {
-            perm: match request.perm.parse() {
-                Ok(perm) => perm,
-                Err(err) => return Err(err.status()),
-            },
-            users: Vec::from_iter(set.into_iter()),
-        };
-
-        let mut store = self.store.clone();
-        match store.create(&acl) {
-            Ok(k) => Ok(Response::new(AclCreateResponse { key: k })),
-            Err(err) => Err(err.status()),
-        }
-    }
-
-    type ListStream = mpsc::Receiver<Result<AclListResponse, Status>>;
-
-    async fn list(
-        &self,
-        request: Request<AclListRequest>,
-    ) -> Result<Response<Self::ListStream>, Status> {
-        let meta = request.metadata();
-
-        if !meta.is_owner() {
-            return Err(Status::unauthenticated("not authorized"));
-        }
-
-        let (mut tx, rx) = mpsc::channel(10);
-        let mut store = self.store.clone();
-
-        tokio::spawn(async move {
-            let iter = match store.list() {
-                Ok(iter) => iter,
-                Err(err) => {
-                    tx.send(Err(err.status())).await.unwrap();
-                    return;
-                }
-            };
-
-            for item in iter {
-                let (key, acl) = match item {
-                    Ok(item) => item,
-                    Err(err) => {
-                        tx.send(Err(err.status())).await.unwrap();
-                        return;
-                    }
-                };
-
-                tx.send(Ok(AclListResponse {
-                    key: key,
-                    acl: Some(Acl {
-                        perm: acl.perm.to_string(),
-                        users: acl.users,
-                    }),
-                }))
-                .await
-                .unwrap();
-            }
-        });
-
-        Ok(Response::new(rx))
-    }
-
-    async fn set(
-        &self,
-        request: Request<AclSetRequest>,
-    ) -> Result<Response<AclSetResponse>, Status> {
-        let meta = request.metadata();
-
-        if !meta.is_owner() {
-            return Err(Status::unauthenticated("not authorized"));
-        }
-
-        let request = request.into_inner();
-        let perm: Permissions = match request.perm.parse() {
-            Ok(perm) => perm,
-            Err(err) => {
-                return Err(Status::invalid_argument(format!(
-                    "failed to parse permission '{}': {}",
-                    request.perm, err
-                )))
-            }
-        };
-
-        let mut store = self.store.clone();
-        let mut acl = match store.get(request.key) {
-            Ok(acl) => match acl {
-                Some(acl) => acl,
-                None => return Err(Status::not_found("no acl found with key")),
-            },
-            Err(err) => return Err(err.status()),
-        };
-
-        acl.perm = perm;
-
-        match store.update(request.key, &acl) {
-            Ok(_) => Ok(Response::new(AclSetResponse {})),
-            Err(err) => Err(err.status()),
-        }
-    }
-
-    async fn grant(
-        &self,
-        request: Request<AclUsersRequest>,
-    ) -> Result<Response<AclUsersResponse>, Status> {
-        let meta = request.metadata();
-
-        if !meta.is_owner() {
-            return Err(Status::unauthenticated("not authorized"));
-        }
-
-        let request = request.into_inner();
-
-        let mut store = self.store.clone();
-        let mut acl = match store.get(request.key) {
-            Ok(acl) => match acl {
-                Some(acl) => acl,
-                None => return Err(Status::not_found("no acl found with key")),
-            },
-            Err(err) => return Err(err.status()),
-        };
-
-        let mut set: HashSet<u64> = HashSet::from_iter(acl.users);
-        let len = set.len();
-        request.users.iter().for_each(|u| {
-            set.insert(*u);
-        });
-
-        let updated = set.len() - len;
-        if updated > 0 {
-            acl.users = Vec::from_iter(set.into_iter());
-
-            match store.update(request.key, &acl) {
-                Ok(_) => Ok(Response::new(AclUsersResponse {
-                    updated: updated as u64,
-                })),
-                Err(err) => Err(err.status()),
-            }
-        } else {
-            Ok(Response::new(AclUsersResponse { updated: 0 }))
-        }
-    }
-    async fn revoke(
-        &self,
-        request: Request<AclUsersRequest>,
-    ) -> Result<Response<AclUsersResponse>, Status> {
-        let meta = request.metadata();
-
-        if !meta.is_owner() {
-            return Err(Status::unauthenticated("not authorized"));
-        }
-
-        let request = request.into_inner();
-
-        let mut store = self.store.clone();
-        let mut acl = match store.get(request.key) {
-            Ok(acl) => match acl {
-                Some(acl) => acl,
-                None => return Err(Status::not_found("no acl found with key")),
-            },
-            Err(err) => return Err(err.status()),
-        };
-
-        let mut set: HashSet<u64> = HashSet::from_iter(acl.users);
-        let len = set.len();
-        request.users.iter().for_each(|u| {
-            set.remove(u);
-        });
-
-        let updated = len - set.len();
-        if updated > 0 {
-            acl.users = Vec::from_iter(set.into_iter());
-
-            match store.update(request.key, &acl) {
-                Ok(_) => Ok(Response::new(AclUsersResponse {
-                    updated: updated as u64,
-                })),
-                Err(err) => Err(err.status()),
-            }
-        } else {
-            Ok(Response::new(AclUsersResponse { updated: 0 }))
-        }
     }
 }
